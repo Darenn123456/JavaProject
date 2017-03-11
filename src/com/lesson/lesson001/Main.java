60c73e0f5836f2f075ec12b6957f457ea39f81c0
DenisMD <markovdenis2012@gmail.com>
2017-03-11 23:08:04 +0500

    Изменил файл 2

--- src/com/lesson/lesson001/Main.java
+++ src/com/lesson/lesson001/Main.java
@@ -3,6 +3,6 @@ package com.lesson.lesson001;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Hello, world, world");
     }
 }
