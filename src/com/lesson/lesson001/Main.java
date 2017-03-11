cf3dfd2f14a284420976cc8c8cd62e2bc766694b
Владимир <vova.rud.00@yandex.ru>
2017-03-11 19:56:21 +0200

    Инициализация проекта

---
+++ src/com/lesson/lesson001/Main.java
@@ -0,0 +1,10 @@
+package com.lesson.lesson001;
+
+/**
+ * Created by Владимир on 11.03.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
