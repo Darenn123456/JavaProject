65a492ca62bf77e22acbed7b505abd32aeee9029
DenisMD <markovdenis2012@gmail.com>
2017-03-11 23:04:45 +0500

    Изменил файл

--- src/com/lesson/lesson001/Main.java
+++ src/com/lesson/lesson001/Main.java
@@ -1,10 +1,8 @@
 package com.lesson.lesson001;
 
-/**
- * Created by Владимир on 11.03.2017.
- */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello, world");
     }
 }
