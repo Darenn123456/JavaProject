4a07eb50bb9baad638669cedcab6945679b20a2e
DenisMD <markovdenis2012@gmail.com>
2017-03-13 23:43:50 +0500

    Home works

---
+++ src/com/lesson/lesson002/home/work/Hw4.java
@@ -0,0 +1,7 @@
+package com.lesson.lesson002.home.work;
+
+//Рассчет силы гравитации
+//https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D0%B0%D1%86%D0%B8%D1%8F
+// Пользователь вводит массу тел и растояние между ними, вычислить силу притяжения
+public class Hw4 {
+}
