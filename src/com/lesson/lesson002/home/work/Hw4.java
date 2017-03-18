de9f429210fba679fa0e78ab6fd4b90c0e5a863f
Владимир <vova.rud.00@yandex.ru>
2017-03-18 16:57:15 +0200

    ДЗ

--- src/com/lesson/lesson002/home/work/Hw4.java
+++ src/com/lesson/lesson002/home/work/Hw4.java
@@ -1,7 +1,31 @@
 package com.lesson.lesson002.home.work;
 
+import java.util.Scanner;
+
 //Рассчет силы гравитации
 //https://ru.wikipedia.org/wiki/%D0%93%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D0%B0%D1%86%D0%B8%D1%8F
 // Пользователь вводит массу тел и растояние между ними, вычислить силу притяжения
 public class Hw4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double G = 6.67e-11;
+        double F;
+        double m1;
+        double m2;
+        double r;
+
+        System.out.print("Введите массу 1 объекта: ");
+        m1 = scanner.nextDouble();
+        System.out.print("Введите массу 2 объекта: ");
+        m2 = scanner.nextDouble();
+        System.out.print("Введите расстояние между объектами: ");
+        r = scanner.nextDouble();
+
+        F = G * ((m1 * m2) / (r * r));
+
+        System.out.println("Гравитационное притяжение этих объектов равняется " + F + " Н");
+
+    }
 }
--- src/com/lesson/lesson002/home/work/Hw5.java
+++ src/com/lesson/lesson002/home/work/Hw5.java
@@ -3,4 +3,12 @@ package com.lesson.lesson002.home.work;
 
 // пользователь вводит с клавиатуры число, напечатать ему какой символ соответствует введеноу числу
 public class Hw5 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
 }
